f15380ae968c35857fb923cdfcdf4d6042237113
kubastra <j.straupisz@gmail.com>
2025-10-23 10:47:07 +0200

    added more details o Book

--- src/main/java/com/example/demo/Book.java
+++ src/main/java/com/example/demo/Book.java
@@ -13,21 +13,51 @@ public class Book {
 	public Integer id;
 	
 	public String title;
+	public String genre;
+	public int year;
+	public String author;
 	
 	public Book() {}
 	
-	public Book(String title) {
+	public Book(String title, String author, String genre, int year) {
 		this.title= title;
+		this.genre = genre;
+		this.year = year;
+		this.author = author;
 	}
+	
 	public int getId() {
 		return id;
+	}
+	
+	public int getYear() {
+		return year;
 	}
 	
 	public String getTitle() {
 		return title;
 	}
 	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
 	void setTitle(String title) {
 		this.title = title;
+	}
+	
+	void setYear(int year) {
+		this.year = year;
+	}
+	
+	void setAuthor(String author) {
+		this.author = author;
 	}
 }
--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import java.security.Provider.Service;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
